9f0ae0fb64d3c0ef38e58d724646bea94ba7e256
XiJordanWang <wang.xi.jordan@gmail.com>
2026-08-01 21:48:25 +1200

    Get accelerate

--- app/src/main/java/com/example/sensorassignment/SensorActivity.kt
+++ app/src/main/java/com/example/sensorassignment/SensorActivity.kt
@@ -5,7 +5,6 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -23,16 +22,21 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.sensorassignment.ui.theme.SensorAssignmentTheme
 
+class AccelerateInfo(val x: Float, val y: Float, val z: Float)
+
 class SensorActivity : ComponentActivity() {
     private lateinit var sensorManager: SensorManager
     private var accelerometer: Sensor? = null
+    private lateinit var sensorListener: SensorEventListener
+
+    private var accelerateInfo: AccelerateInfo = AccelerateInfo(0f, 0f, 0f)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         getAccelerometerValue()
         setContent {
-            Activities()
+            Activities(accelerateInfo)
         }
     }
 
@@ -40,12 +44,40 @@ class SensorActivity : ComponentActivity() {
     private fun getAccelerometerValue() {
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorListener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent) {
+                val xAxis = event.values[0]
+                val yAxis = event.values[1]
+                val zAxis = event.values[2]
+
+                accelerateInfo = AccelerateInfo(xAxis, yAxis, zAxis)
+            }
+
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+                // Handle accuracy changes if needed
+            }
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        // Register the sensor listener
+        accelerometer?.let {
+            sensorManager.registerListener(sensorListener, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // Delete the sensor listener to save battery
+        sensorManager.unregisterListener(sensorListener)
     }
 }
 
 
 @Composable
-fun Activities() {
+fun Activities(accelerateInfo: AccelerateInfo) {
     Column(modifier = Modifier.fillMaxSize()) {
         for (i in 1..6) {
             ElevatedCard(
@@ -55,27 +87,34 @@ fun Activities() {
                 modifier = Modifier
                     .size(width = 240.dp, height = 100.dp)
             ) {
-                CardInfo("Sensor$i")
+                CardInfo("Accelerometer", accelerateInfo)
             }
         }
     }
 }
 
 @Composable
-fun CardInfo(title: String) {
+fun <T> CardInfo(title: String, details: T) {
     Text(
         text = title,
         modifier = Modifier
             .padding(16.dp),
         textAlign = TextAlign.Center,
     )
-    Text(text = "parameter1:")
+    when (details) {
+        is AccelerateInfo -> {
+            Text(text = "X: ${details.x}")
+            Text(text = "Y: ${details.y}")
+            Text(text = "Z: ${details.z}")
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ActivitiesPreview() {
     SensorAssignmentTheme {
-        Activities()
+        Activities(accelerateInfo = AccelerateInfo(0f, 0f, 0f))
     }
 }
