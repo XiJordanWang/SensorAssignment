4e4f1e4b46df11e3456afa20e7aa38105bda10c0
XiJordanWang <wang.xi.jordan@gmail.com>
2026-08-03 11:59:17 +1200

    Finish first page's layout

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 I found Layouts introduction in the documentation showed
 on https://developer.android.com/develop/ui/compose/layouts/basics
-1. I asked Gemini to help me introduce the way to navigete
 
+1. I asked Gemini to help me introduce the way to navigete
 
 
 2. I ask Gemini about how to write get sensor value and why is it.
 
 ![accelerometer_info](screenshot/accelerometer_info.png)
 
-
 3. When I tried using object to refresh the page, there is no changes. So I asked Gemini
 
+4.How can I get my new picture as MainActivities background picture?
--- app/src/main/java/com/example/sensorassignment/MainActivity.kt
+++ app/src/main/java/com/example/sensorassignment/MainActivity.kt
@@ -5,22 +5,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.sensorassignment.ui.theme.SensorAssignmentTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,10 +41,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SensorAssignmentTheme {
-                Scaffold(modifier = Modifier.fillMaxSize().fillMaxHeight()) { innerPadding ->
-                    Greeting(
-                        modifier = Modifier.padding(innerPadding)
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Image(
+                        painter = painterResource(id = R.drawable.heart_beat_is_shown_black_background),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop, // This makes the image fill the screen
+                        modifier = Modifier.fillMaxSize()
                     )
+                    Scaffold(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .fillMaxHeight(),
+                        containerColor = Color.Transparent
+                    ) { innerPadding ->
+                        Greeting(
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
@@ -42,35 +67,53 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
     val rainbowColors: List<Color> = listOf(
-        Color.Red, Color.Yellow, Color.Cyan
+        Color.Yellow, Color.Cyan, Color.Magenta
     )
-    Column() {
-        // https://developer.android.com/develop/ui/compose/text/style-text
-        Text(
-            text = buildAnnotatedString {
-                append("Hello, there!\n")
-                append("This is")
-                withStyle(
-                    SpanStyle(
-                        brush = Brush.linearGradient(
-                            colors = rainbowColors
-                        )
-                    )
-                ) {
-                    append(" Xi Wang's ")
-                }
-                append("assignment!")
-            },
-            modifier = modifier
-        )
 
-        // https://developer.android.com/develop/ui/compose/components/button
-        val context = LocalContext.current
-        ElevatedButton(onClick = {
-            val intent = Intent(context, SensorActivity::class.java)
-            context.startActivity(intent)
-        }) {
-            Text("Sensor Activity")
+    // Using AI here for layout
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
+        Column(
+            modifier = Modifier
+                .padding(start = 24.dp, end = 24.dp, bottom = 128.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            // https://developer.android.com/develop/ui/compose/text/style-text
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(
+                        SpanStyle(
+                            brush = Brush.linearGradient(
+                                colors = rainbowColors
+                            ),
+                            fontStyle = FontStyle.Italic
+                        )
+                    ) {
+                        append(" Xi Wang's")
+                    }
+                    append(" assignment!")
+                },
+                modifier = modifier,
+                color = Color.White,
+                fontSize = 32.sp
+            )
+
+            // https://developer.android.com/develop/ui/compose/components/button
+            val context = LocalContext.current
+            ElevatedButton(
+                onClick = {
+                    val intent = Intent(context, SensorActivity::class.java)
+                    context.startActivity(intent)
+                }, colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFFF5722),
+                    contentColor = Color.White,
+                ),
+                modifier = Modifier
+                    .size(width = 207.dp, height = 64.dp)
+            ) {
+                Text(text = "Sensor Activity", fontSize = 16.sp)
+            }
         }
     }
 }
